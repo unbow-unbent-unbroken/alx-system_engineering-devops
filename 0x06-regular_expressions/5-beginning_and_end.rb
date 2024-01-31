a46d3e02f6bfe35017d64342d8a59e877e81c542
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-01-30 18:35:56 -0800

    hot fix task5

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # A regular expression that matches a string that starts with h and ends with n and canhave any single character in between.
-puts ARGV[0].scan(/\h.\b/).join
+puts ARGV[0].scan(/h.b/).join
