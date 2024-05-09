0a6032436d13cc6ec6ed1781c9fb54fd8b1afe27
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-05-09 08:06:41 +0100

    hot fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ import requests
 def numbers_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'MyBot/0.0.1'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
