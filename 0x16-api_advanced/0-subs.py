5d75f962aa337f9c3b7d000a762d3a3965e1302a
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-05-09 08:05:08 +0100

    pep requirement fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 A script that queries the Reddit API and returns the number of subscribers
 (not active users, total subscribers) for a given subreddit
@@ -6,9 +7,10 @@ A script that queries the Reddit API and returns the number of subscribers
 
 import requests
 
+
 def numbers_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent':'MyBot/0.0.1'}
+    headers = {'User-Agent': 'MyBot/0.0.1'}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
