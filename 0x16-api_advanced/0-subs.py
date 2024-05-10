15035bde0922da534a294d8e9e8aa8b5dd57b9a0
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-05-10 10:06:52 +0100

    using other aspect

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-
 """
-A script that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit
+number of subscribers for a given subreddit
 """
 
-import requests
+from requests import get
 
 
-def numbers_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Google Chrome Version 124.0.6367.119'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    """
 
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
